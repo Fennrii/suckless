7031a0de0da8c681941af5a750fd79393958a830
Fennrii <axsateagle@gmail.com>
2026-02-25 17:32:42 -0500

    Changed battery % to be an average of all batteries

--- slstatus/scripts/battery.sh
+++ slstatus/scripts/battery.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-if [ -f "/sys/class/power_supply/BAT0/capacity" ]; then
-  echo "$(cat /sys/class/power_supply/BAT0/capacity)%" 
+total=0; found=0; 
+for bat in /sys/class/power_supply/BAT*/capacity; do 
+  [ -f "$bat" ] && total=$((total + $(cat "$bat"))) && found=$((found + 1)); 
+done; 
+if [ "$found" -gt 0 ]; then
+  echo "$((total/found))%"
 else
   echo "Plug"
 fi
